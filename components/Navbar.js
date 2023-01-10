edd5688d7755401948c6393a25c4262ea7c1e1bd
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2023-01-10 16:57:17 +0530

    Navbar Links Updation Completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,29 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import logo from "../public/assets/navLogo.png";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const [shadow, setShadow] = useState(false);
+    const [navBg, setNavBg] = useState("#ecf0f3");
+    const [linkColor, setLinkColor] = useState("#1f2937");
+    const router = useRouter();
+
+    useEffect(() => {
+        if (
+            router.asPath === "/property" ||
+            router.asPath === "/crypto" ||
+            router.asPath === "/netflix" ||
+            router.asPath === "/twitch"
+        ) {
+            setNavBg("transparent");
+            setLinkColor("#ecf0f3");
+        } else {
+            setNavBg("#ecf0f3");
+            setLinkColor("#1f2937");
+        }
+    }, [router]);
 
     const handleNav = () => {
         setNav(!nav);
@@ -27,6 +46,7 @@ const Navbar = () => {
 
     return (
         <div
+            style={{ backgroundColor: `${navBg}` }}
             className={
                 shadow
                     ? "fixed w-full h-20 shadow-xl z-[100]"
@@ -38,7 +58,10 @@ const Navbar = () => {
                     <Image src={logo} alt="Logo" width="75" height="50" />
                 </Link>
                 <div>
-                    <ul className="hidden md:flex">
+                    <ul
+                        style={{ color: `${linkColor}` }}
+                        className="hidden md:flex"
+                    >
                         <Link href="/#home">
                             <li className="ml-10 text-sm uppercase hover:border-b">
                                 Home
