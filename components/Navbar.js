c81d76e29fd820d4e9cace0f1a462c8dd5c89c23
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2023-01-10 16:32:07 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -90,7 +90,14 @@ const Navbar = () => {
                 >
                     <div>
                         <div className="flex w-full items-center justify-between">
-                            <Image src={logo} alt="/" width="67" height="35" />
+                            <Link href="/">
+                                <Image
+                                    src={logo}
+                                    alt="/"
+                                    width="67"
+                                    height="35"
+                                />
+                            </Link>
                             <div
                                 onClick={handleNav}
                                 className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
@@ -107,19 +114,44 @@ const Navbar = () => {
                     <div className="py-4 flex flex-col">
                         <ul className="uppercase">
                             <Link href="/#home">
-                                <li className="py-4 text-sm">Home</li>
+                                <li
+                                    onClick={() => setNav(false)}
+                                    className="py-4 text-sm"
+                                >
+                                    Home
+                                </li>
                             </Link>
                             <Link href="/#about">
-                                <li className="py-4 text-sm">About</li>
+                                <li
+                                    onClick={() => setNav(false)}
+                                    className="py-4 text-sm"
+                                >
+                                    About
+                                </li>
                             </Link>
                             <Link href="/#skills">
-                                <li className="py-4 text-sm">Skills</li>
+                                <li
+                                    onClick={() => setNav(false)}
+                                    className="py-4 text-sm"
+                                >
+                                    Skills
+                                </li>
                             </Link>
                             <Link href="/#projects">
-                                <li className="py-4 text-sm">Projects</li>
+                                <li
+                                    onClick={() => setNav(false)}
+                                    className="py-4 text-sm"
+                                >
+                                    Projects
+                                </li>
                             </Link>
                             <Link href="/#contact">
-                                <li className="py-4 text-sm">Contact Us</li>
+                                <li
+                                    onClick={() => setNav(false)}
+                                    className="py-4 text-sm"
+                                >
+                                    Contact Us
+                                </li>
                             </Link>
                         </ul>
                         <div className="pt-16">
