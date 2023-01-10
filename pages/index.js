7e4bd941e00ec81e14561e9ace57d15d74836b00
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2023-01-10 16:33:19 +0530

    Constant Navbar along the websiteCreated

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
-import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 // import { Inter } from "@next/font/google";
@@ -27,7 +26,6 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Navbar />
             <Main />
             <About />
             <Skills />
